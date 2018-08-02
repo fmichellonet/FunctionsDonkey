8f597313cec8f0a6f48f2c03935d1be3bd4cc37c
Fabrice Michellonet <fmichellonet@scop-it.com>
2018-08-02 19:21:49 +0200

    some minor refactor

---
+++ FunctionsDonkey.Msbuild.Generator/Class1.cs
@@ -0,0 +1,9 @@
+﻿//using System;
+//using Microsoft.Build.Framework.Utilities;
+
+//namespace FunctionsDonkey.Msbuild.Generator
+//{
+//    public class AzFunctionGenerator : Microsoft.Build.Utilities.Task
+//    {
+//    }
+//}
